fcb6fee1a4919c5a87a9c992851f90770a75486f
new-name <dligui.ghizlane@gmail.com>
2023-10-28 11:40:51 -0700

    m

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
+# installs puppet-lint package
+
 package {'flask':
   ensure   => '2.1.0',
+  name     => 'flask',
   provider => 'pip3'
 }
